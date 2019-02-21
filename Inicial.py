017550e6c1152b757f843dd49790ea4829c31102
A. Carlos Pinheiro <prog.antoniocarlos@outlool.com>
2019-02-21 01:21:51 -0300

    Inicial

---
+++ Inicial.py
@@ -0,0 +1,17 @@
+
+# PS C:\Dev\Python> python
+# Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> print('Hello World')
+# Hello World
+# >>> hello = 'Hello World'
+# >>> print(hello)
+# Hello World
+# >>> exit
+# Use exit() or Ctrl-Z plus Return to exit
+# >>> exit()
+# PS C:\Dev\Python> python inicial.py
+# Hello World
+# PS C:\Dev\Python>
+
+print('Hello World')
