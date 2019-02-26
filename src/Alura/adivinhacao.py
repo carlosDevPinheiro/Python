b3f38d3ea7a69200b232cf3eea6c88e5e7699a0b
A. Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2019-02-26 17:03:58 -0300

    Adicionando níveis ao jogo

--- src/Alura/adivinhacao.py
+++ src/Alura/adivinhacao.py
@@ -1,15 +1,66 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************",end="\n")
 
-numero_secreto = 42
-chute = input('Digite o seu numero: ')
-print('Voce digitou o numero: ',chute)
+numero_secreto = random.randrange(1,101)
+rodada = 1
+tentativas = 42
 
-if numero_secreto == int(chute):
-    print('Voce acertou !')
-else:
-    print('voce errou :( ')
+# Usando FOR  
+# Obs. if,elif,while não precisa utilizar o parenteses 
+ 
+for rodada in range(1, tentativas + 1):
+    print(numero_secreto)
+    print("Tentativa {} de {}".format(rodada, tentativas))
+    chute_str = input("Digite seu Número: ")
+    print("Você digitou: ",chute_str)
+    chute = int(chute_str)
 
+    if chute < 1 or chute > 100:
+        print('Voce deve digitar um número entre 1 e 100.')
+        continue  # pula a interação
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto    
+
+    if acertou :
+        print('Voce acertou !')
+        break
+    else:
+        if menor:
+            print('Voce errou ! Seu chute foi menor que o numero secreto.')
+        elif maior:
+            print('Voce errou ! Seu chute foi maior que o numero secreto.')
 
 print("Fim do Jogo")
+
+
+# Parei na Aula:  Nível e Pontuação > Adicionando níveis ao jogo
+
+
+
+# Usando WHILE
+# while(rodada <= tentativas):    
+#     #print('Tentativa: ', rodada," de ",tentativas)
+#     print("Tentativa {} de {}".format(rodada, tentativas))
+#     chute_str = input("Digite seu Número: ")
+#     print("Você digitou: ",chute_str)
+#     chute = int(chute_str)
+
+#     acertou = chute == numero_secreto
+#     maior   = chute > numero_secreto
+#     menor   = chute < numero_secreto
+
+#     if acertou :
+#         print('Voce acertou !')
+#     else:
+#         if menor:
+#             print('Voce errou ! Seu chute foi menor que o numero secreto.')
+#         elif maior:
+#             print('Voce errou ! Seu chute foi maior que o numero secreto.')
+#     rodada = rodada + 1
+       
+  
